71ea4072ea326e0300c65e1e003c5f68998d86f6
apogeeai <2078853+apogeeai@users.noreply.github.com>
2024-12-28 22:51:02 +0000

    Fixed AI

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a728f8d0a039fe3e"; }
+/******/ 		__webpack_require__.h = function() { return "cdc7c3f8dfba34c9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
