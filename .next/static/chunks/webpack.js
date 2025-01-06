26992a36824d3d3e8db1b93a77951551efee1a65
apogeeai <2078853+apogeeai@users.noreply.github.com>
2025-01-06 20:15:01 +0000

    Automatic commit before Assistant edits

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d3d5635fd8a9dfdc"; }
+/******/ 		__webpack_require__.h = function() { return "d15b95aacf4e9eaf"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
