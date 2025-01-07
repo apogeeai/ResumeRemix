822ec7aa60615cb447cd512972bb78d499ad4ce9
apogeeai <2078853+apogeeai@users.noreply.github.com>
2025-01-07 00:22:08 +0000

    Added Cover Letters

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1f7caaf9d577e4da"; }
+/******/ 		__webpack_require__.h = function() { return "2f42867573dc985d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
